b415e1371196e681ac7aac41d3e6e8443d79ff03
ameks94 <ameks94@gmail.com>
2016-09-25 14:55:22 +0300

    init commit

---
+++ src/main/java/com/labs/Launcher.java
@@ -0,0 +1,13 @@
+package com.labs;
+
+/**
+ * Hello world!
+ *
+ */
+public class Launcher
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/labs/Test.java
@@ -0,0 +1,10 @@
+package com.labs;
+
+import junit.framework.Assert;
+
+/**
+ * Created by ameks on 25.09.2016.
+ */
+public class Test extends Assert {
+
+}
